fe0ef0525b096ee35ab1a5778aa831fdd5486c8a
Drishti Priya <71181509+Drishti2002@users.noreply.github.com>
2021-12-10 13:03:53 +0530

    Weather app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import WeatherInfo from "./Components/WeatherInfo";
+
+const App = () =>{
+  
+  return (
+      <>
+        <WeatherInfo/>
+      </>
+  );
+}
+
+export default App;
